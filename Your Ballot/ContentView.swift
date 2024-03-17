4676201d5342e1009bfa63cc721e827855aa44d4
Peter Herman <20931612+peteherman@users.noreply.github.com>
2024-03-17 15:12:05 -0400

    Initial Commit

---
+++ Your Ballot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Your Ballot
+//
+//  Created by Peter Herman on 3/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Your Ballot/Your_BallotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Your_BallotApp.swift
+//  Your Ballot
+//
+//  Created by Peter Herman on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Your_BallotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
